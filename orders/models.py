b389e001e0e4ce468117d9ca70e55a7040e34c30
Sardor Irgashev <irgashevsardor99@gmail.com>
2022-05-24 18:01:41 +0500

    Alter database column

--- orders/models.py
+++ orders/models.py
@@ -5,7 +5,7 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     # идентификатор price для получения стоимости товара (новый метод stripe для работы с платежами)
-    price_id = models.CharField(max_length=50)
+    price = models.FloatField()
 
     def __str__(self):
         return self.name
--- orders/views.py
+++ orders/views.py
@@ -18,12 +18,17 @@ def create_checkout_session(request, pk):
             checkout_session = stripe.checkout.Session.create(
                 line_items=[
                     {
-                        # Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-                        'price': item.price_id,
+                        'price_data': {
+                            'currency': 'usd',
+                            'product_data': {
+                                'name': item.name,
+                            },
+                            'unit_amount': int(item.price * 100),
+                        },
                         'quantity': 1,
-                    },
-                ],
+                    }],
                 mode='payment',
+
                 success_url=DOMAIN + '/success',
                 cancel_url=DOMAIN + '/cancel',
             )
